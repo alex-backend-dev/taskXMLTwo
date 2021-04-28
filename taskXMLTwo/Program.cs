b30a82eb417c8d87324eb8d6c34c78dfc7de455f
Alex Ulyanitskiy <adl821440@gmail.com>
2021-04-28 19:54:38 +0300

    Add project files.

---
+++ taskXMLTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace taskTwoXML
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] content = File.ReadAllLines("Inlet.in.txt", Encoding.Default);
+
+            XDocument xDoc = new XDocument();
+
+            XElement root = new XElement("root");
+            XElement line = new XElement("line");
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (content[i].StartsWith("data:"))
+                {
+                    root.Add(new XProcessingInstruction("instr", content[i].Substring(5)));
+                    continue;
+                }
+
+                line.Add(content[i]);
+                root.Add(line);
+
+                line = new XElement("line");
+            }
+
+            xDoc.Add(root);
+            xDoc.Save("resultFile.xml");
+        }
+    }
+}
